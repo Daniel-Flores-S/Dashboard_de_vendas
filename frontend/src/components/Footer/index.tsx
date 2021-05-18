2bfbaa912ebeced9eef02f5a5556db6945de5f4e
Daniel <danielfloresda802@gmail.com>
2021-05-18 16:46:24 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+
+const Footer = () => {
+    return(
+        <footer className = "footer mt-auto py bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Daniel-Flores-S" target="_blank" rel="noreferrer"> Daniel Flores Santos</a></p>
+                <p className="text-light"><small><strong>Dashboard Vendas</strong><br /></small></p>
+            </div>
+        </footer>
+);
+}
+
+export default Footer;
